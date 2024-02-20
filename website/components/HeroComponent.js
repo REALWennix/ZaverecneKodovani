9620b351c3d679702cd934ce9345191edf45355e
Bruno0pockejsi0Niubo <127151236+Bruno0pockejsi0Niubo@users.noreply.github.com>
2024-02-20 21:57:55 +0100

    Update HeroComponent.js
    JS hero

--- website/components/HeroComponent.js
+++ website/components/HeroComponent.js
@@ -1,21 +1,18 @@
 Vue.component('hero-component', {
   template: `
     <div class="hero">
-      <div class="flex-container">
-        <div class="image-container">
-          <img class="d-block w-100" src="/website/imgs/data/Post-1.jpg" alt="Static Image">
-        </div>
-        <div class="content">
-          <h2>Title</h2>
-          <p>Description</p>
-          <button @click="readMore">Read More</button>
-        </div>
+      <div class="overlay"></div>
+      <img src="/website/imgs/data/Post-1.jpg" alt="Hero Image">
+      <div class="hero-content">
+        <h2>Výhra v soutěži</h2>
+        <p>Čtveřice studentů vyhrála v soutě...</p>
+        <button @click="readMore">Číst dále</button>
       </div>
     </div>
   `,
   methods: {
     readMore() {
-      console.log('Read More button clicked');
+      console.log('Read More clicked');
     }
   }
-});
+}); 
