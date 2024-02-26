9cf9659dbb115f5e5080dd5b1e2d034daae94807
Vojtěch Kat Mydlář <127151171+Busnaa@users.noreply.github.com>
2024-02-26 20:38:56 +0100

    Create HeroComponent.js

---
+++ website/HeroComponent.js
@@ -0,0 +1,18 @@
+Vue.component('hero-component', {
+  template: `
+    <div class="hero">
+      <div class="overlay"></div>
+      <img src="/website/imgs/data/Post-1.jpg" alt="Hero Image">
+      <div class="hero-content">
+        <h2>Výhra v soutěži</h2>
+        <p>Čtveřice studentů vyhrála v soutě...</p>
+        <button @click="readMore">Číst dále</button>
+      </div>
+    </div>
+  `,
+  methods: {
+    readMore() {
+      console.log('Read More clicked');
+    }
+  }
+});
