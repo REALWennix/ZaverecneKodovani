36124eb3d38e12797035093e084417d1a9e5f03d
REALWennix <rosmartin63@gmail.com>
2024-02-28 16:27:19 +0100

    Added Footer

--- website/components/FooterComponent.js
+++ website/components/FooterComponent.js
@@ -1,7 +1,16 @@
 Vue.component('footer-component', {
   template: `
     <footer>
-      <p>&copy; 2024 Martin Rosič</p>
+      <div class="foot">
+        <img class="foot-logo" src="imgs/foot.png" alt="Left Icon">
+        <div class="foot-content">
+          <a class="footer-link" href="/">Střední průmyslová škola elektrotechnická, Praha 2, Ječná 30</a>, Ječná 30, 120 00, Praha 2<br>
+          Zřizovatel školy: <a class="footer-link" href="http://www.praha.eu">Hlavní město Praha</a><br>
+          Tel/Fax: 224 941 469 | E-mail: <a class="footer-link" href="mailto:spsejecna@spsejecna.cz">spsejecna@spsejecna.cz</a> | <a class="footer-link" href="/">www.spsejecna.cz</a> | <a class="footer-link" href="http://linkedin.com/school/spse-jecna">linkedin.com/school/spse-jecna</a><br>
+          <span class="copy">Provozuje SPŠE Ječná © Všechna práva vyhrazena.</span> <span class="print-fix">ochrana všech publikovaných materiálů</span> | <a class="footer-link" href="/prohlaseni-o-pristupnosti">Prohlášení o přístupnosti</a> | <a class="footer-link" href="/ochrana-soukromi#cookies">Cookies</a> | <a class="footer-link" href="/ochrana-soukromi">Ochrana soukromí</a>
+        </div>
+        <img class="foot-logo" src="imgs/cez_logo2.png" alt="Right Icon">
+      </div>
     </footer>
   `
 });
