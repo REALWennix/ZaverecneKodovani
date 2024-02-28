aa2898015a1a4188b3230d8a196d286218b54162
REALWennix <rosmartin63@gmail.com>
2024-02-28 16:33:55 +0100

    changed links

--- website/components/NavbarComponent.js
+++ website/components/NavbarComponent.js
@@ -1,11 +1,11 @@
 Vue.component('navbar-component', {
-    props: ['isMenuOpen'],
-    template: `
+  props: ['isMenuOpen'],
+  template: `
     <nav class="navbar" :class="{ 'open': isMenuOpen }">
       <ul>
-        <li><a href="#">PRO UCHAZEČE</a></li>
-        <li><a href="#">PRO ŽÁKY A RODIČE</a></li>
-        <li><a href="#">PRO ZAMĚSTNANCE</a></li>
+        <li><a href="https://www.spsejecna.cz/prijimaci-rizeni">PRO UCHAZEČE</a></li>
+        <li><a href="https://www.spsejecna.cz/">PRO ŽÁKY A RODIČE</a></li>
+        <li><a href="https://www.spsejecna.cz/">PRO ZAMĚSTNANCE</a></li>
       </ul>
     </nav>
   `
